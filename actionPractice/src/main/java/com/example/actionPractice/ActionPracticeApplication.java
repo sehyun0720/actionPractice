2e9f5e3e05f8a96e67c8870152b051321784c245
sehyun0720 <ttapp52580@naver.com>
2024-02-27 17:43:03 +0900

    first Commit

---
+++ actionPractice/src/main/java/com/example/actionPractice/ActionPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.actionPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionPracticeApplication.class, args);
+	}
+
+}
---
+++ actionPractice/src/test/java/com/example/actionPractice/ActionPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actionPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
