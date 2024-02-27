a8175a6ebc5746c931b1fbc3fecea2955f23da35
sehyun0720 <ttapp52580@naver.com>
2024-02-27 17:49:17 +0900

    controller update2

--- actionPractice/src/main/java/com/example/actionPractice/controller/TestController.java
+++ actionPractice/src/main/java/com/example/actionPractice/controller/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TestController {
 
-    @GetMapping("/test")
+    @GetMapping("/testString")
     public String getTestString() {
         return "testString";
     }
